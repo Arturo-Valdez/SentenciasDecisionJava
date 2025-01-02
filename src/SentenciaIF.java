17d8a24c4a567e6fd12b0f99d62db92b5ab1702a
Arturo Valdez <santiguardado27@gmail.com>
2025-01-01 19:48:02 -0600

    Sentencia IF

---
+++ src/SentenciaIF.java
@@ -0,0 +1,12 @@
+public class SentenciaIF {
+    public static void main(String[] args) {
+
+        var edad = 30;
+
+        if(edad >= 18){
+            System.out.println("Eres mayor de edad");
+        }else {
+            System.out.println("Eres menor de edad");
+        }
+    }
+}
